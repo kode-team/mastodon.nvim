9f58a1c2ddd5f602379b2222e34f1af80daf9224
Jaeyeol Lee <rijgndqw012@gmail.com>
2022-11-24 15:50:25 +0900

    feat: Add sqlite functionality in order to manage multiple fedi-verse accounts

---
+++ lua/mastodon/db_client.lua
@@ -0,0 +1,91 @@
+local vim = vim
+
+local has_sqlite, sqlite = pcall(require, "sqlite")
+if not has_sqlite then
+  error("This plugin requires sqlite.lua (https://github.com/kkharji/sqlite.lua) " .. tostring(sqlite))
+end
+
+local db_table = {}
+db_table.mastodon_accounts = "mastodon_accounts"
+
+
+local M = {}
+
+
+function M:new()
+  local o = {}
+  setmetatable(o, self)
+  self.db = nil
+  return o
+end
+
+function M:reset_database()
+  self:bootstrap(nil)
+  return self.db:with_open(function(db)
+    return db:delete("mastodon_accounts")
+  end)
+end
+
+function M:bootstrap(db_root)
+  if self.db then return end
+
+  local project_env = os.getenv("PROJECT_ENV") or "production"
+  db_root = db_root or vim.fn.stdpath('data')
+  local db_name = 'mastodon_accounts'
+  if project_env == 'test' then
+    db_name = db_name .. '_test'
+  end
+
+  local db_filename = db_root .. "/" .. db_name .. ".sqlite3"
+  self.db = sqlite:open(db_filename)
+
+  if not self.db then
+    vim.notify("Mastodon.nvim: error in opening DB", vim.log.levels.ERROR)
+    return
+  end
+
+  local first_run = false
+
+  if not self.db:exists(db_table.mastodon_accounts) then
+    first_run = true
+    self.db:create(db_table.mastodon_accounts, {
+      id           = {"INTEGER", "PRIMARY", "KEY"},
+      instance_url = {"TEXT"},
+      access_token = {"TEXT"},
+      username     = {"TEXT"},
+      description  = {"TEXT"},
+      is_active    = {"BOOLEAN"},
+    })
+  end
+
+  self.db:close()
+  return first_run
+end
+
+function M:get_all_accounts()
+  self:bootstrap(nil)
+  return self.db:with_open(function(db)
+    return db:select("mastodon_accounts", {})
+  end)
+end
+
+function M:get_active_account()
+  return self:select_account({ is_active = true })
+end
+
+function M:add_account(params)
+  self:bootstrap(nil)
+  return self.db:with_open(function(db)
+    return db:insert("mastodon_accounts", params)
+  end)
+end
+
+function M:select_account(params)
+  self:bootstrap(nil)
+  return self.db:with_open(function(db)
+    return db:select("mastodon_accounts", params)
+  end)
+end
+
+
+return M
---
+++ tests/mastodon/db_client_spec.lua
@@ -0,0 +1,36 @@
+
+local db_client = require("mastodon.db_client")
+
+describe("setup", function()
+  before_each(function()
+    db_client:bootstrap()
+    db_client:reset_database()
+  end)
+
+  it("Added accounts can be displayed in select view", function()
+    db_client:add_account({
+      username = 'hello',
+      access_token = 'hello',
+      instance_url = 'hello',
+      description = 'world',
+      is_active = false,
+    })
+    db_client:add_account({
+      username = 'hello',
+      access_token = 'hello',
+      instance_url = 'hello',
+      description = 'world',
+      is_active = false,
+    })
+    db_client:add_account({
+      username = 'hello',
+      access_token = 'hello',
+      instance_url = 'hello',
+      description = 'world',
+      is_active = false,
+    })
+
+    local accounts = db_client:get_all_accounts()
+    assert(table.getn(accounts) == 3)
+  end)
+end)
