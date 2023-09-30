c0d1f9fd308c4d3fc5639992a29324f019b19118
malkoG <rijgndqw012@gmail.com>
2023-09-30 17:37:54 +0900

    Support configurable keymap
    closes #19
    
    This commit updates the keymaps and operations configuration in the Mastodon
     plugin. The keymaps are now defined in a separate Lua table, making it easier
     to modify or extend them. The operations, which represent the actions triggered
     by the keymaps, are also defined in a separate table for improved clarity and
     maintainability.
    
    Here are the keymap changes:
    
    - Buffer-wide keymaps:
      - `reload-statuses` is mapped to `,mr`
      - `scroll-to-top` is mapped to `,mk`
      - `scroll-to-bottom` is mapped to `,mj`
    
    - Status-wide keymaps:
      - `reply` is mapped to `,tr`
      - `bookmark` is mapped to `,tb`
      - `favourite` is mapped to `,tf`
      - `boost` is mapped to `,tB`
      - `print` is mapped to `,tv`
    
    Here are the system-wide keymaps:
    
    - `home-timeline` is mapped to `,mh`
    - `bookmarks` is mapped to `,mb`
    - `favourites` is mapped to `,mf`
    - `mentions` is mapped to `,mR`
    - `post-message` is mapped to `,mw`
    - `select-account` is mapped to `,ms`
    
    These changes enhance the flexibility and customizability of the Mastodon plugin.

--- lua/mastodon.lua
+++ lua/mastodon.lua
@@ -2,16 +2,64 @@
 local commands = require("mastodon.commands")
 
 local M = {}
-M.config = {
-  -- default config
-  opt = "Hello!",
+
+
+M.default_keymaps = {
+  ["buffer-wide-keymaps"] = {
+    ["reload-statuses"] = ",mr",
+    ["scroll-to-top"] = ",mk",
+    ["scroll-to-bottom"] = ",mj",
+
+    ["reply"] = ",tr",
+    ["bookmark"] = ",tb",
+    ["favourite"] = ",tf",
+    ["boost"] = ",tB",
+    ["print"] = ",tv",
+  },
+  ["system-wide-keymaps"] = {
+    ["home-timeline"] = ",mh",
+    ["bookmarks"] = ",mb",
+    ["favourites"] = ",mf",
+    ["mentions"] = ",mR",
+
+    ["post-message"] = ",mw",
+    ["select-account"] = ",ms",
+  },
+}
+
+M.operations = {
+  ["buffer-wide-keymaps"] = {
+    ["reload-statuses"] = ":lua require('mastodon').reload_statuses()<CR>",
+    ["scroll-to-top"] = ":lua require('mastodon.commands').fetch_newer_statuses()<CR>",
+    ["scroll-to-bottom"] = ":lua require('mastodon.commands').fetch_older_statuses()<CR>",
+
+    ["reply"] = ":lua require('mastodon.actions').reply()<CR>",
+    ["bookmark"] = ":lua require('mastodon.actions').toggle_bookmark()<CR>",
+    ["favourite"] = ":lua require('mastodon.actions').toggle_favourite()<CR>",
+    ["boost"] = ":lua require('mastodon.actions').toggle_boost()<CR>",
+    ["print"] = ":lua require('mastodon.actions').print_verbose_information()<CR>",
+  },
+  ["system-wide-keymaps"] = {
+    ["home-timeline"] = ":lua require('mastodon').fetch_home_timeline()<CR>",
+    ["bookmarks"] = ":lua require('mastodon').fetch_bookmarks()<CR>",
+    ["favourites"] = ":lua require('mastodon').fetch_favourites()<CR>",
+    ["mentions"] = ":lua require('mastodon').fetch_replies()<CR>",
+
+    ["post-message"] = ":lua require('mastodon').toot_message()<CR>",
+    ["select-account"] = ":lua require('mastodon').select_account()<CR>",
+  },
+}
+
+M.default_config = {
+  keymaps = M.default_keymaps
 }
 
 -- setup is the public method to setup your plugin
 M.setup = function(args)
   -- you can define your setup function here. Usually configurations can be merged, accepting outside params and
   -- you can also put some validation here for those.
-  M.config = vim.tbl_deep_extend("force", M.config, args or {})
+  M.config = vim.tbl_deep_extend("force", M.default_config, args or {})
+  vim.g.mastodon_config = M.config
 
   namespace = vim.api.nvim_create_namespace("MastodonNS")
   local hl_for_whitespace = vim.api.nvim_get_hl(0, { name = "NormalFloat" })
